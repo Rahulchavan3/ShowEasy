4d106913c6c6d94b8ca678c423a475eaa444c2fb
rahulchavan3 <rahulchavan3030@gmail.com>
2023-01-04 16:28:55 +0530

    Started working on Movie page

--- src/App.js
+++ src/App.js
@@ -1,21 +1,26 @@
-import './App.css';
-//Routing
-import {Routes , Route} from 'react-router-dom';
-//React slick css
-import "slick-carousel/slick/slick.css"; 
+import "./App.css";
+import axios from "axios";
+// Routing
+import { Routes, Route } from "react-router-dom";
+// React slick css
+import "slick-carousel/slick/slick.css";
 import "slick-carousel/slick/slick-theme.css";
-//pages
+// Pages
 import HomePage from "./pages/Home.page";
 import MoviePage from "./pages/Movie.page";
-import PlayPage from './pages/Play.page';
+import PlayPage from "./pages/Play.page";
+
+axios.defaults.baseURL = "https://api.themoviedb.org/3";
+axios.defaults.params = {};
+axios.defaults.params["api_key"] = process.env.REACT_APP_API_KEY;
 
 function App() {
-  return <Routes>
+  return (
+    <Routes>
       <Route path="/" element={<HomePage />} />
       <Route path="/movie/:id" element={<MoviePage />} />
       <Route path="/plays" element={<PlayPage />} />
-  </Routes>
- 
+    </Routes>
+  );
 }
-
 export default App;
